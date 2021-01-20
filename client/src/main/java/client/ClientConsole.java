5518b7acdbf7e072b6f5f6a24314465e2a89980e
Oleg <olegrikov@ya.ru>
2021-01-20 19:27:15 +0300

    hw6

--- client/src/main/java/client/ClientConsole.java
+++ client/src/main/java/client/ClientConsole.java
@@ -1,4 +1,50 @@
 package client;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ClientConsole {
+    private Socket socket;
+    private final String IP_ADDRESS = "localhost";
+    private final int PORT = 8189;
+
+    public ClientConsole() {
+        try {
+            socket = new Socket(IP_ADDRESS, PORT);
+            Scanner sc = new Scanner(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            Scanner scMsg = new Scanner(System.in);
+
+            new Thread(() -> {
+                while (true) {
+                    String str = sc.nextLine();
+
+                    if (str.equals("/end")) {
+                        break;
+                    }
+                    System.out.println("Server: " + str);
+                }
+            }).start();
+
+            while (true) {
+                String clientMsg = scMsg.nextLine();
+                System.out.println("You: " + clientMsg);
+
+                if (clientMsg.equals("/end")) {
+                    break;
+                }
+                out.println(clientMsg);
+            }
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,4 +1,61 @@
 package server;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Server {
+    static ServerSocket server;
+    static Socket socket;
+    static final int PORT = 8189;
+
+    public static void main(String[] args) {
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Server started");
+            socket = server.accept();
+            System.out.println("Client connected");
+
+            Scanner sc = new Scanner(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            Scanner scMsg = new Scanner(System.in);
+
+            new Thread(() -> {
+                while (true) {
+                    String str = sc.nextLine();
+                    if (str.equals("/end")) {
+                        System.out.println("client disconnected");
+                        break;
+                    }
+                    System.out.println("Client: " + str);
+                }
+            }).start();
+
+            while (true) {
+                String serverMsg = scMsg.nextLine();
+                System.out.println("You: " + serverMsg);
+
+                if (serverMsg.equals("/end")) {
+                    break;
+                }
+
+                out.println(serverMsg);
+            }
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
